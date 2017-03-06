78e96d03a5c7e09b20c3363db4a4e2afd6976e97
BPH <ba.perlaki@gmail.com>
2017-03-06 01:19:55 +0100

    Create main WebserviceRAC class

---
+++ BPHWebserviceRACSwift/WebserviceRAC.swift
@@ -0,0 +1,13 @@
+//
+//  WebserviceRAC.swift
+//  BPHWebserviceRACSwift
+//
+//  Created by Balazs Perlaki-Horvath on 06/03/17.
+//  Copyright © 2017 perlakidigital. All rights reserved.
+//
+
+import Foundation
+
+class WebserviceRAC {
+
+}
